6fe0455dee2e20728e0d97e3b96099a89b8ee81e
andy.ninety9 <duy.maianh26@gmail.com>
2024-11-19 21:10:05 +0700

    Push: Create new event method

--- EventManagement/src/model/Event.java
+++ EventManagement/src/model/Event.java
@@ -6,12 +6,12 @@ package model;
 
 import utils.MyLib;
 
-
 /**
  *
  * @author andymai
  */
 public class Event {
+
     private String event_id;
     private String event_name;
     private String event_date;
@@ -20,9 +20,8 @@ public class Event {
     private int number_of_attendees;
     private String event_status;
 
-
     public Event() {
-        
+
     }
 
     public Event(String event_id, String event_name, String event_date, String location, int number_of_attendees, String event_status) {
@@ -81,29 +80,62 @@ public class Event {
     public void setEvent_status(String event_status) {
         this.event_status = event_status;
     }
-    
-    public void create_new_event(){
+
+    public boolean create_new_event() {
         this.event_id = MyLib.generateID("E");
-//        Exxxxxx
+
         this.event_name = MyLib.getString("Enter event's name: ");
+//        Check name >= 5 character
+        if (this.getEvent_name().length() < 5) {
+            System.out.println("Error message: Event name is at least five characters long");
+            return false;
+        }
+//        Check name does not contain spaces
+        String temp_name = this.getEvent_name();
+        char[] tmp_name_arr = temp_name.toCharArray();
+        for (char c : tmp_name_arr) {
+            if (c == ' ') {
+                System.out.println("Error message: Event name does not contain spaces");
+                return false;
+            }
+        }
+
         this.event_date = MyLib.getString("Enter event's date: ");
+        //        Check date format "yyyy/mm/dd"
+        String temp_date = this.getEvent_date();
+        if (!temp_date.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")) {
+            System.out.println("Error message: Event date must be follow instructor (yyyy-mm-dd)");
+            return false;
+        }
+
         this.location = MyLib.getString("Enter event's location: ");
-        while (true) {            
+        while (true) {
             try {
                 this.number_of_attendees = MyLib.getIntegerNumber("Enter num of attendees: ");
+                //        Check Number of attendees must be greater than 0
+                if (this.getNumber_of_attendees() <= 0) {
+                    System.out.println("Error message: Number of attendees must be greater than 0");
+                    return false;
+                }
                 break;
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
-        
+
         this.event_status = MyLib.getString("Enter event's status: ");
+        //        Check Status is either \"Available\" or \"Not Available\"
+        if(!this.getEvent_status().equals("Available") 
+                && !this.getEvent_status().equals("Not Available")){
+            System.out.println("Error message: Status is either Available or Not Available");
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
         return "Event{" + "event_id=" + event_id + ", event_name=" + event_name + ", event_date=" + event_date + ", location=" + location + ", number_of_attendees=" + number_of_attendees + ", event_status=" + event_status + '}';
     }
-    
-    
+
 }
